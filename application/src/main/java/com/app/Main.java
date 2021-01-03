34d3eb5ade6a6f93580f4efe46a196fac70928de
Stas Tarasov <iluvstrwrs@mail.ru>
2021-01-03 17:41:21 +0300

    init

---
+++ application/src/main/java/com/app/Main.java
@@ -0,0 +1,11 @@
+package com.app;
+
+import com.lib.Lib;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Lib lib = new Lib();
+        lib.sayHelloWorld();
+    }
+}
---
+++ library/src/main/java/com/lib/Lib.java
@@ -0,0 +1,8 @@
+package com.lib;
+
+public class Lib {
+
+    public void sayHelloWorld(){
+        System.out.println("Hello world");
+    }
+}
